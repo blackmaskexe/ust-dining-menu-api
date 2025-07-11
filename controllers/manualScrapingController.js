2e1f6ce6f24a6725493700dc8483393972deae6f
Blackmask <82077507+blackmaskexe@users.noreply.github.com>
2025-07-11 14:52:00 +0000

    send early response in manual scraping controller to assure user of success

--- controllers/manualScrapingController.js
+++ controllers/manualScrapingController.js
@@ -1,6 +1,14 @@
-const { scrapeViewMenu, scrapeNorthsiderMenu } = require("./scrapeMenu");
+const {
+  scrapeViewMenu,
+  scrapeNorthsiderMenu,
+} = require("../scraping/scrapeMenu");
 
 exports.manuallyScrapeMenu = (req, res) => {
+  res.json({
+    message: "Scraping was started. You will see the updated menu shortly!",
+    completionTime: "1-2 minutes",
+  });
+
   scrapeViewMenu();
   scrapeNorthsiderMenu();
 };
